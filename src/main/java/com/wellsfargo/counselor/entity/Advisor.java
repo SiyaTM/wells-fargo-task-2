2e4abb53638edf12d78d18c71917deb681a98aca
SiyaTM <siyatmahlangu@gmail.com>
2024-01-04 04:58:19 +0200

    Class for entity

--- src/main/java/com/wellsfargo/counselor/entity/Advisor.java
+++ src/main/java/com/wellsfargo/counselor/entity/Advisor.java
@@ -1,16 +1,19 @@
 package com.wellsfargo.counselor.entity;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
 @Entity
 public class Advisor {
 
     @Id
-    @GeneratedValue()
+    @GeneratedValue
+    @JsonIgnore
     private long advisorId;
 
     @Column(nullable = false)
@@ -28,10 +31,13 @@ public class Advisor {
     @Column(nullable = false)
     private String email;
 
+    // Default constructor for JPA
     protected Advisor() {
 
     }
 
+    // Constructor initializing all instance variables
+    @JsonIgnore
     public Advisor(String firstName, String lastName, String address, String phone, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -40,47 +46,60 @@ public class Advisor {
         this.email = email;
     }
 
-    public Long getAdvisorId() {
+    // Getter and setter methods
+    @JsonIgnore
+    public long getAdvisorId() {
         return advisorId;
     }
 
+    @JsonIgnore
     public String getFirstName() {
         return firstName;
     }
 
+    @JsonIgnore
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    @JsonIgnore
     public String getLastName() {
         return lastName;
     }
 
+    @JsonIgnore
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    @JsonIgnore
     public String getAddress() {
         return address;
     }
 
+    @JsonIgnore
     public void setAddress(String address) {
         this.address = address;
     }
 
+    @JsonIgnore
     public String getPhone() {
         return phone;
     }
 
+    @JsonIgnore
     public void setPhone(String phone) {
         this.phone = phone;
     }
 
+    @JsonIgnore
     public String getEmail() {
         return email;
     }
 
+    @JsonIgnore
     public void setEmail(String email) {
         this.email = email;
     }
 }
+
